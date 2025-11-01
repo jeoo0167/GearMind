1809ee0f4b36a2ede4b66ea0fc20b140e983aa1e
jeoo0167 <jesusenriqueolveraochoa1@gmail.com>
2025-11-01 12:02:18 -0600

    Changes to be committed: 	modified:   src/DebugMsg.cpp 	modified:   src/DebugMsg.h
    created a function was plot the mpu data

--- src/DebugMsg.h
+++ src/DebugMsg.h
@@ -4,31 +4,57 @@
 #include <Arduino.h>
 #include <stdarg.h>
 
+
 /*
-Structure
+-------------STRUCTURE------------
+
+-------------Messages--------------
 DebugMsgs debug("class.cpp",false);
 debug(debug.INFO,"Hello Wolrd");
 -------------OUTPUT----------------
 [class.cpp][INFO][Hello World]
+
+------------Plotter-----------------
+DebugMsgs debug("class.cpp",false);
+debug.plot({"tag1","tag2"},value1,value2);
+-------------OUTPUT-----------------
+>tag1,value1,tag2,value2
+
 */
+
 class DebugMsgs {
-public:
-    enum msg_type { INFO, DEBUG, WARN, ERROR };
+    public:
+        enum msg_type { INFO, DEBUG, WARN, ERROR };
 
-    // Constructor: name = nombre de la clase / módulo, colors = activar colores
-    explicit DebugMsgs(const String& name, bool colors = false)
-        : filename(name), useColors(colors) {}
+        // Constructor: name = nombre de la clase / módulo, colors = activar colores
+        explicit DebugMsgs(const String& name, bool colors = false)
+            : filename(name), useColors(colors) {}
 
-    void msg(msg_type type, const char* format, ...);
+        void msg(msg_type type, const char* format, ...);
 
-private:
-    String filename;
-    bool useColors;
+        template <typename... Args>
+        static void plot(const String names[], const Args&... values)
+        {
+            const String vals[] = { String(values)... };  // convierte todos a String
+            constexpr size_t n = sizeof...(values);
 
-    char buffer[64];
+            Serial.print(">");
+            for (size_t i = 0; i < n; ++i) {
+                Serial.print(names[i]);
+                Serial.print(":");
+                Serial.print(vals[i]);
+                if (i < n - 1) Serial.print(",");
+            }
+            Serial.println();
+        }
+    private:
+        String filename;
+        bool useColors;
 
-    const char* typeToStr(msg_type type);
-    const char* typeToColor(msg_type type);
+        char buffer[64];
+
+        const char* typeToStr(msg_type type);
+        const char* typeToColor(msg_type type);
 };
 
 #endif
